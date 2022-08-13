262e0831aaad64388a05c19f34a0c23af3886262
adamhatton <adamdhatton@gmail.com>
2022-08-13 13:24:20 +0000

    Add LearnerProfile to admin site and configure display list

--- learner_account/admin.py
+++ learner_account/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import LearnerProfile
 
-# Register your models here.
+
+@admin.register(LearnerProfile)
+class LearnerProfileAdmin(admin.ModelAdmin):
+    '''
+    Adds LearnerProfile model into the admin site
+    '''
+    list_display = ('user_name', 'first_name', 'last_name', 'phone', 'ability', 'shortened_about')
+
+    @admin.display(description='Username')
+    def user_name(self, obj):
+        '''
+        Gets the username from the User model
+        '''
+        return obj.user.username
+   
+    @admin.display(description='Name')
+    def first_name(self, obj):
+        '''
+        Gets the first name from the User model
+        '''
+        return obj.user.first_name
+
+    @admin.display(description='Surname')
+    def last_name(self, obj):
+        '''
+        Gets the username from the User model
+        '''
+        return obj.user.last_name
+
+    @admin.display(description='About')
+    def shortened_about(self, obj):
+        '''
+        Shortens the about me info for display in admin panel
+        '''
+        return obj.about[:100]
--- learner_account/models.py
+++ learner_account/models.py
@@ -9,11 +9,23 @@ class LearnerProfile(models.Model):
     '''
     Model to extend user and include additional learner information
     '''
-    ABILITY_CHOICES = [('','Choose your level'),('B', 'Beginner'),('I','Intermediate'),('A', 'Advanced')]
+    ABILITY_CHOICES = [('', 'Choose your level'), ('B', 'Beginner'), ('I', 'Intermediate'), ('A', 'Advanced')]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = PhoneNumberField(blank=True)
-    ability = models.CharField(max_length=1,choices=ABILITY_CHOICES)
+    ability = models.CharField(max_length=1, choices=ABILITY_CHOICES, blank=True)
     about = models.TextField(blank=True)
 
     def __str__(self):
         return f'Profile for {self.user.username}'
+
+
+
+@receiver(post_save, sender=User)
+def create_learner_profile(sender, instance, created, **kwargs):
+    '''
+    Create and ave the LearnerProfile whenever a User is created.
+    '''
+    if created:
+        LearnerProfile.objects.create(user=instance)
+    
+    instance.learnerprofile.save()
