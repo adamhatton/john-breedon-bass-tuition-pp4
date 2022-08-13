e93317c130f04d96d63dc5483bd858d794ecf3f5
adamhatton <adamdhatton@gmail.com>
2022-08-13 11:17:33 +0000

    Add LearnerProfile model to extend User model

---
+++ learner_account/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Div
+from crispy_bootstrap5.bootstrap5 import FloatingField
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    '''
+    Creates an instance of the ContactForm to be passed to a template
+    '''
+
+    class Meta:
+        '''
+        Specifies the fields to be included in the ContactForm class
+        '''
+        model = Contact
+        fields = ('name', 'email', 'phone', 'message')
+
+    def __init__(self, *args, **kwargs):
+        '''
+        Creates a FormHelper to enable layout changes in crispy forms
+        '''
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+        self.helper.layout = Layout(
+            FloatingField('name'),
+            FloatingField('email'),
+            FloatingField('phone', placeholder='Phone'),
+            'message',
+            Div(
+                Submit('submit', 'Submit'),
+                css_class='center-button'
+            ),
+        )
--- learner_account/models.py
+++ learner_account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class LearnerProfile(models.Model):
+    '''
+    Model to extend user and include additional learner information
+    '''
+    ABILITY_CHOICES = [('','Choose your level'),('B', 'Beginner'),('I','Intermediate'),('A', 'Advanced')]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = PhoneNumberField(blank=True)
+    ability = models.CharField(max_length=1,choices=ABILITY_CHOICES)
+    about = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Profile for {self.user.username}'
