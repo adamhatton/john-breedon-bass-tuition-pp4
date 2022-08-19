d4af970277b8c1c07244f09458f6b49266792ca0
adamhatton <adamdhatton@gmail.com>
2022-08-19 21:55:53 +0000

    Add logic to prevent duplicate bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
+from .models import Booking
 from .forms import BookingForm
 
 
@@ -29,7 +30,15 @@ class BookingsPage(View):
         booking_form = BookingForm(request.POST)
         booking_form.instance.user = request.user
 
-        if booking_form.is_valid():
+        date = request.POST['date']
+        time = request.POST['time']
+
+        # Check if booking already exists
+        if Booking.objects.filter(date=date).filter(time=time).exists():
+            messages.error(request, 'That slot is unavailable, please select a different time')
+            return redirect('/bookings/')
+
+        elif booking_form.is_valid():
             booking_form.save()
             messages.success(request, 'Your lesson is booked!')
             return redirect('/bookings/')
