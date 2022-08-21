c04ec6dcb877422c90af79619c23c5ab402ce7c6
adamhatton <adamdhatton@gmail.com>
2022-08-21 18:30:04 +0000

    Add class view for editing bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib import messages
 import availability
@@ -50,6 +50,58 @@ class BookingsPage(View):
             request,
             'bookings.html',
             {
+                'booking_form': booking_form,
+            }
+        )
+
+class EditBooking(View):
+    '''
+    Renders the bookings page with a specific instance of a booking for amending
+    '''
+    def get(self, request, booking_id):
+        '''
+        Finds the booking to edit and renders the bookings page
+        '''
+        booking_to_edit = get_object_or_404(Booking, pk=booking_id)
+        booking_form = BookingForm(instance=booking_to_edit)
+        booking_availability = availability.get_booking_availability()
+
+        return render(
+            request,
+            'bookings.html',
+            {
+                'booking_availability': booking_availability,
+                'booking_form': booking_form,
+            }
+        )
+
+    def post(self, request, booking_id):
+        '''
+        Handles POST requests to the bookings page
+        '''
+        booking_to_edit = get_object_or_404(Booking, pk=booking_id)
+        booking_form = BookingForm(request.POST, instance=booking_to_edit)
+
+        date = request.POST['date']
+        time = request.POST['time']
+
+        # Check if booking already exists
+        if Booking.objects.filter(date=date).filter(time=time).exists():
+            messages.error(request, 'That slot is unavailable, please select a different time')
+            return redirect(f'/bookings/edit_booking/{booking_id}')
+
+        elif booking_form.is_valid():
+            booking_form.save()
+            messages.success(request, 'Your lesson has been successfully changed!')
+            return redirect('/learner_account/')
+
+        booking_availability = availability.get_booking_availability()
+
+        return render(
+            request,
+            'bookings.html',
+            {
+                'booking_availability': booking_availability,
                 'booking_form': booking_form,
             }
         )
