88257e243de5d206e33013d9f7eea06390c0dd25
adamhatton <adamdhatton@gmail.com>
2022-08-24 13:05:44 +0000

    Add unit tests for bookings.forms.py

---
+++ bookings/test_forms.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .forms import BookingForm
+
+
+class TestBookingForm(TestCase):
+    '''
+    Class for testing the Booking Form
+    '''
+
+    def test_booking_required_fields(self):
+        '''Tests that date, time and type are required in booking form'''
+        form = BookingForm({
+            'date': '',
+            'time': '',
+            'type': '',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('date', form.errors.keys())
+        self.assertIn('time', form.errors.keys())
+        self.assertIn('type', form.errors.keys())
+
+    def test_booking_fields_are_explicit_in_form_metaclass(self):
+        '''Tests that only fields specified in the meta are displayed'''
+        form = BookingForm()
+        self.assertEqual(
+            form.Meta().fields,
+            ('date', 'time', 'phone', 'type')
+        )
+
+    def test_booking_date_field_uses_date_widget(self):
+        '''Tests that the date field uses a date widget'''
+        form = BookingForm()
+        self.assertEqual(form.fields['date'].widget.__class__.__name__, 'DateInput')
